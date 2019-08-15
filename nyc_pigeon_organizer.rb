66391c5f13df6159edb6e22ac79c833fa13d8126
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-15 12:11:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,15 @@
-def nyc_pigeon_organizer(pigeon_data)
-  pigeon_data= hash.new{|hash, key| hash[key]}
+def nyc_pigeon_organizer(data)
+hash={}
+data.each do |colors_gender_lives,values|
+  values.each do |value,array||
+    array.each do |name|
+      if hash[name]==nil
+        hash[name]= {}
+        hash[name][colors_gender_lives]=[]
+      else
+        hash[name][colors_gender_lives] = []
+      end
+    end
+  end
 end
-  p pigeon_data
-end
+hash.each do
